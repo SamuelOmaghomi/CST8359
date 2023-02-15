c5d8ff911ab17683f28204d4c49a0a4fdd917cf6
Samuel Omaghomi <omag0003@algonquinlive.com>
2023-02-15 15:24:29 -0500

    Finished Lab3

---
+++ Lab3/Lab3/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab3.Models;
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SongForm() => View();
+
+        [HttpPost]
+        public IActionResult Sing()
+        {
+            //sets the users input for number of bottles
+            HttpContext.Session.SetString("countBottles", Request.Form["countBottles"]);
+
+            return View();
+        }
+
+        public IActionResult CreateStudent() => View();
+
+        [HttpPost]
+       public IActionResult DisplayStudent(Student student)
+        {
+            return View(student);
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Lab3/Lab3/Models/Student.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab3.Models
+{
+    public class Student
+    {
+        [Required]
+        public String firstName
+        {
+            get; set;
+        }
+        [Required]
+        public String lastName
+        {
+            get; set;
+        }
+        [Required]
+        public int studentId
+        {
+            get; set;
+        }
+        [Required]
+        public String emailAddress
+        {
+            get; set;
+        }
+        [Required]
+        public String password
+        {
+            get; set;
+        }
+        [Required]
+        public String studentDescription
+        {
+            get; set;
+        }
+    }
+}
