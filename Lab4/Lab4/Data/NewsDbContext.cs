0f04393c4c8b17fb428f493c28a3c83c81475914
Samuel Omaghomi <omag0003@algonquinlive.com>
2023-03-15 14:08:25 -0400

    Finished Lab4

---
+++ Lab4/Lab4/Data/NewsDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Lab4.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab4.Data
+{
+    public class NewsDbContext : DbContext
+    {
+        public NewsDbContext(DbContextOptions<NewsDbContext> options) : base(options) { }
+        public DbSet<NewsBoard> NewsBoards { get; set;}
+        public DbSet<Client> Clients { get; set;}
+        public DbSet<Subscription> Subscriptions { get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Client>().ToTable("Client");
+            modelBuilder.Entity<NewsBoard>().ToTable("NewsBoard");
+            modelBuilder.Entity<Subscription>()
+                .HasKey(s => new { s.ClientId, s.NewsBoardId });
+            modelBuilder.Entity<Subscription>().ToTable("Subscription");
+        }
+    }
+}
---
+++ Lab4/Lab4/Models/Client.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Lab4.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+
+        
+        [Required]
+        [DisplayName("LastName")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [DisplayName("FirstName")]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Birth Date")]
+        public DateTime BirthDate { get; set; }
+
+        public string FullName { get { return (LastName + ", " + FirstName); } }
+
+        public ICollection<Subscription> Subscriptions { get; set; }
+        
+    }
+}
